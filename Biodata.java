b2232262181e810bf0c4a0bf11227f91256fb459
maidiyangsa <57142580+maidiyangsa@users.noreply.github.com>
2020-10-18 14:09:59 +0800

    Biodata

---
+++ Biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Beta Three
+ * NAMA     : ADRIAN MAIDIYANGSA
+ * KELAS    : IF-1
+ * NIM      : 10119029
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+       
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Adrian Maidiyangsa");
+        System.out.println("ALAMAT\t\t: Jl.Raya Pantura No 18");
+        System.out.println("TEMPAT LAHIR\t\t: Pontianak");
+        System.out.println("TANGGAL LAHIR\t\t: 28 Mei 1999");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Child");
+        System.out.println("MIKES\t\t: Nothing");
+    }
+    
+}
